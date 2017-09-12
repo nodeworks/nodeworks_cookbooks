69936776c96c08308880aaca7cbf4d5fc4c07efb
Rob Lee <rob@nodeworks.com>
2017-09-12 01:58:03 -0400

    changes

--- cookbooks/nodeworks_opsworks/metadata.rb
+++ cookbooks/nodeworks_opsworks/metadata.rb
@@ -19,7 +19,6 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 #
 # source_url 'https://github.com/<insert_org_here>/nodeworks_opsworks'
 
-depends 'chef_nginx'
 depends 'chef_slack'
 depends 'application_git'
 depends 'certbot'
