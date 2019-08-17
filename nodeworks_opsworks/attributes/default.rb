e7c2fc9d9521ac961b4afd031e12ef9627b2e189
Rob Lee <rob@nodeworks.com>
2019-08-17 14:44:05 -0400

    slack update

--- nodeworks_opsworks/attributes/default.rb
+++ nodeworks_opsworks/attributes/default.rb
@@ -1,3 +1,3 @@
-default['slack']['webhook_url'] = 'https://hooks.slack.com/services/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'
+default['slack']['webhook_url'] = 'https://hooks.slack.com/services/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'
 node.default[:alb_helper][:connection_draining_timeout] = 750
 node.default[:alb_helper][:state_check_frequency] = 30
