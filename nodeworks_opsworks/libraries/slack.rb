6955d352ee2f4f593904eef08e542ee25472664d
Rob Lee <rob@nodeworks.com>
2019-08-17 15:05:28 -0400

    slack update

--- nodeworks_opsworks/libraries/slack.rb
+++ nodeworks_opsworks/libraries/slack.rb
@@ -2,7 +2,7 @@ require 'slack-notifier'
 
 class Chef::Recipe::NodeworksNotify
   def self.send_notification(text)
-    notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
+    notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
     notifier.ping text
   end
 end
